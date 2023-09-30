e0531245b681f9042432ed96a9f808520140c71a
dlaurianti <dlaurianti@uiowa.edu>
2023-09-30 16:39:52 -0500

    feat: routes now use OAuth2 forms

--- backend/routes.py
+++ backend/routes.py
@@ -1,46 +1,50 @@
-from fastapi import FastAPI, APIRouter, Depends, HTTPException
+from fastapi import FastAPI, APIRouter, Depends, HTTPException, Header
 from fastapi.security.oauth2 import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
 from datetime import datetime, timedelta
 from auth import create_token, decode_token, revoke_token, revoked_tokens
 from models import verify_login, register_user
 
 router = APIRouter()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
 
-oauth2 = OAuth2PasswordBearer(tokenUrl="/login")
 
-@router.post("/register")
-async def register(form: OAuth2PasswordRequestForm = Depends()):
-    if not register_user(form.username, form.password):
-        raise HTTPException(status_code=400, detail="User already exists")
-    return {"registered": True}
+class Credentials(BaseModel):
+    username: str
+    password: str
+
 
 @router.post("/login")
-async def login(form: OAuth2PasswordRequestForm = Depends()):
+async def login(credentials: OAuth2PasswordRequestForm = Depends()):
     # validate username and password
-    if not verify_login(form.username, form.password):
+    if not verify_login(credentials.username, credentials.password):
         raise HTTPException(status_code=401, detail="Invalid Credentials")
 
     # generate access token
     user_info = {
-        "user": form.username,
-        "scope": "read"
+        "user": credentials.username,
     }
     token = create_token(user_info, expires_delta=timedelta(minutes=15))
-    return {"access_token": token}
+    return {"token": token}
+
 
 @router.post("/logout")
-def logout(token: str = Depends(oauth2)):
+def logout(token: str = Depends(oauth2_scheme)):
     decoded_token = decode_token(token)
     if decoded_token is None:
         raise HTTPException(status_code=401, detail="Invalid Token")
-    
+
     revoke_token(token)
     return {"logged_out": True}
 
+
 @router.get("/secret")
-def secret(token: str = Depends(oauth2)):
+def secret(token: str = Depends(oauth2_scheme)):
     decoded_token = decode_token(token)
-    if decoded_token is None or decoded_token in revoked_tokens:
+    if decoded_token is None or token in revoked_tokens:
         raise HTTPException(status_code=401, detail="Invalid Token")
-    
-    return {"verified" : True, "user_info": decoded_token.get("user")}
+    with open("decoded_store.txt", "r") as f:
+        secret_data = f.read()
+    return {"verified": True,
+            "user_info": decoded_token.get("user"),
+            "secret_data": secret_data}
