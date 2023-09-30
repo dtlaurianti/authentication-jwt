7673fb8aac68b49bf5861e2fa619c3961e35d162
dlaurianti <dlaurianti@uiowa.edu>
2023-09-30 16:41:57 -0500

    feat: App now uses react-router-dom protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import LandingPage from "./components/LandingPage";
+import LoginForm from "./components/LoginForm";
+import Unauthorized from "./components/Unauthorized";
+import Secret from "./components/Secret";
+import RequireAuth from "./components/RequireAuth";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<LoginForm />} />
+      <Route path="/unauthorized" element={<Unauthorized />} />
+
+      <Route element={<RequireAuth />}>
+        <Route path="/secret" element={<Secret />} />
+      </Route>
+
+      <Route path="*" element={<h1>404 Not Found</h1>} />
+    </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
