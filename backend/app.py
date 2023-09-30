6c1b3dd953fd89923b67eaa0bffa80d391ec128a
dlaurianti <dlaurianti@uiowa.edu>
2023-09-30 09:22:50 -0500

    feat: app imports routes and starts server

--- backend/app.py
+++ backend/app.py
@@ -1,2 +1,10 @@
+#!/usr/bin/env python3
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from routes import router
+
+app = FastAPI()
+app.include_router(router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=8000)
