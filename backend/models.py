10ca4122ea8918746906373fe5e37ef28e251bfc
dlaurianti <dlaurianti@uiowa.edu>
2023-09-30 09:24:47 -0500

    feat: login verification and registration model logic

--- backend/models.py
+++ backend/models.py
@@ -1,14 +1,17 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 import json
+from database.orm_models import Base, UserLoginInfo
 
 with open("config.json", "r") as f:
-    config = json.load(f)
+    config = json.load(f)["database"]
+
+print(config)
 
 connection = (
     f'mysql+mysqlconnector://{config["user"]}:'
     f'{config["password"]}@{config["host"]}:'
-    f'{config["port"]}/{config["database"]}'
+    f'{config["port"]}/{config["database_name"]}'
 )
 
 engine = create_engine(connection)
@@ -22,3 +25,19 @@ def session_decorator(func):
         session.close()
         return res
     return wrapper
+
+@session_decorator
+def verify_login(session: Session, username: str, password: str):
+    user = session.query(UserLoginInfo).filter_by(username=username).first()
+    if user is None:
+        return False
+    return user.password == password
+    
+@session_decorator
+def register_user(session: Session, username: str, password: str):
+    user = session.query(UserLoginInfo).filter_by(username=username).first()
+    if user is not None:
+        return False
+    session.add(UserLoginInfo(username=username, password=password))
+    session.commit()
+    return True
