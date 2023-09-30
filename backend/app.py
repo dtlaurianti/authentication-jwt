c056a38b6b8cfdb9033bed694e61ce323c58651d
dlaurianti <dlaurianti@uiowa.edu>
2023-09-30 16:15:11 -0500

    feat: added CORS support

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python3
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes import router
 
 app = FastAPI()
 app.include_router(router)
+
+# CORS
+# --------------------------------------------------------------
+origins = [
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     import uvicorn
